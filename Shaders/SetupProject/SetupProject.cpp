caab2b508cc361cb054aa19488cf32adc03975ae
Rizumuwu <miguel_paglinawan@dlsu.edu.ph>
2023-05-18 12:25:50 +0800

    shadebun

---
+++ Shaders/SetupProject/SetupProject.cpp
@@ -0,0 +1,217 @@
+// SetupProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <math.h>
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h";
+
+#include <string>
+#include <iostream>
+
+float x_mod = 0;
+float y_mod = 0;
+
+void Key_CallBack(
+    GLFWwindow* window,
+    int key,
+    int scancode,
+    int action,
+    int mod
+)
+{
+    if (key == GLFW_KEY_D && action == GLFW_PRESS) {
+        x_mod += 0.1f;
+    }
+    else if (key == GLFW_KEY_A && action == GLFW_PRESS) {
+        x_mod -= 0.1f;
+    }
+    else if (key == GLFW_KEY_W && action == GLFW_PRESS) {
+        y_mod += 0.1f;
+    }
+    else if (key == GLFW_KEY_S && action == GLFW_PRESS) {
+        y_mod -= 0.1f;
+    }
+}
+int main(void)
+{
+    GLFWwindow* window;
+    float i = 0;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(500, 500, "Miguel Paglinawan", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    
+    glfwSetKeyCallback(window, Key_CallBack);
+
+    std::fstream vertSrc("Shaders/sample.vert");
+    std::stringstream vertBuff;
+    vertBuff << vertSrc.rdbuf();
+    std::string vertS = vertBuff.str();
+    const char* v = vertS.c_str();
+
+    std::fstream fragSrc("Shaders/sample.frag");
+    std::stringstream fragBuff;
+    fragBuff << fragSrc.rdbuf();
+    std::string fragS = fragBuff.str();
+    const char* f = fragS.c_str();
+
+    GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertShader, 1, &v, NULL);
+    glCompileShader(vertShader);
+
+    GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragShader, 1, &f, NULL);
+    glCompileShader(fragShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertShader);
+    glAttachShader(shaderProgram, fragShader);
+
+    glLinkProgram(shaderProgram);
+
+    std::string path = "3D/bunny.obj";
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> material;
+    std::string warning, error;
+
+    tinyobj::attrib_t attributes;
+
+    bool success = tinyobj::LoadObj(
+        &attributes,
+        &shapes,
+        &material,
+        &warning,
+        &error,
+        path.c_str()
+    );
+
+    std::vector<GLuint> mesh_indices;
+    
+    for (int i = 0; i < shapes[0].mesh.indices.size(); i++) {
+        mesh_indices.push_back(
+            shapes[0].mesh.indices[i].vertex_index
+        );
+    }
+
+    GLfloat vertices[]{
+        //x    y    z
+        0.f, 0.5f, 0.f,     // point 1
+        -0.5f, -0.5f, 0.f,  // point 2
+        0.5f, -0.5f, 0.f    // point 3
+    };
+
+    GLuint indices[]{
+        0, 1, 2
+    };
+
+    GLuint VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        sizeof(GL_FLOAT) * attributes.vertices.size(),
+        &attributes.vertices[0],
+        GL_STATIC_DRAW
+    );
+
+    glVertexAttribPointer(
+        0,  // 0 == position
+        3,  // xyz
+        GL_FLOAT, // what array is type of
+        GL_FALSE,
+        3 * sizeof(GL_FLOAT),
+        (void*)0
+    );
+
+    //enable index 0
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+        sizeof(GLuint) * mesh_indices.size(),
+        mesh_indices.data(),
+        GL_STATIC_DRAW
+    );
+
+    // tells opengl that you're done with VAO and VBO;
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        unsigned int xLoc = glGetUniformLocation(shaderProgram, "x");
+        glUniform1f(xLoc, x_mod);
+
+        unsigned int yLoc = glGetUniformLocation(shaderProgram, "y");
+        glUniform1f(yLoc, y_mod);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glDrawElements(
+            GL_TRIANGLES,
+            mesh_indices.size(),
+            GL_UNSIGNED_INT,
+            0
+        );
+
+        /*
+        glBegin(GL_POLYGON);
+        glVertex2f(0.0f, 0.4f);
+        glVertex2f(0.3f, 0.2f);
+        glVertex2f(0.2f, -0.1f);
+        glVertex2f(-0.2f, -0.1);
+        glVertex2f(-0.3f, 0.2f);
+        glEnd();
+        */
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glfwTerminate();
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
